53286b1978714b9aa58e0e32d50c38ddf1df2e6b
kschat <kyleschattler@gmail.com>
2025-12-05 00:38:06 -0500

    feat: day 3 part 2

--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -5,9 +5,9 @@ use crate::{cli::RunConfig, error::Error, problem::Problem};
 pub struct Day3;
 
 impl<'a> Problem<'a> for Day3 {
-    type Input = Vec<Vec<u32>>;
-    type Answer1 = u32;
-    type Answer2 = usize;
+    type Input = Vec<Vec<u64>>;
+    type Answer1 = u64;
+    type Answer2 = u64;
 
     fn init(_config: &'a RunConfig) -> Self
     where
@@ -22,7 +22,7 @@ impl<'a> Problem<'a> for Day3 {
             .filter(|line| !line.trim().is_empty() && !line.starts_with('#'))
             .map(|line| {
                 line.chars()
-                    .map(|char| char.to_digit(10).unwrap())
+                    .map(|char| char.to_digit(10).unwrap() as u64)
                     .collect()
             })
             .collect();
@@ -41,11 +41,7 @@ impl<'a> Problem<'a> for Day3 {
                             return (first, second);
                         }
 
-                        if second < first {
-                            (*digit, first)
-                        } else {
-                            (*digit, second)
-                        }
+                        (*digit, if second < first { first } else { second })
                     },
                 );
 
@@ -55,4 +51,32 @@ impl<'a> Problem<'a> for Day3 {
 
         Ok(result)
     }
+
+    fn part2(&self, input: &Self::Input) -> Result<Self::Answer2, Error> {
+        let result = input.iter().map(|bank| select_n_batteries(bank, 12)).sum();
+
+        Ok(result)
+    }
+}
+
+fn select_n_batteries(bank: &[u64], units: usize) -> u64 {
+    bank.iter()
+        .rev()
+        .skip(units)
+        .fold(bank[bank.len() - units..].to_vec(), |mut acc, digit| {
+            let mut digit = *digit;
+            let mut index = 0;
+
+            while index < acc.len() && acc[index] <= digit {
+                (digit, acc[index]) = (acc[index], digit);
+                index += 1;
+            }
+
+            acc
+        })
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(exp, digit)| digit * 10_u64.pow(exp as u32))
+        .sum()
 }
